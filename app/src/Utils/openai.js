201cfed0bf5a8f42d7b7044e07b022e3b3128491
Ayush Gupta <ayushgupta@192.168.1.10>
2025-08-07 00:07:40 +0530

    update open router api key

--- app/src/Utils/openai.js
+++ app/src/Utils/openai.js
@@ -1,14 +1,14 @@
 const OpenAI = require("openai");
 
 const openai = new OpenAI({
-  baseURL: 'https://openrouter.ai/api/v1',
-  apiKey: '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'
+  baseURL: 'https://api.perplexity.ai',
+  apiKey: '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'
 });
 
 const openAIFunctions = {
   analysisQueryFromAi: async(query, content) => {
     const completion = await openai.chat.completions.create({
-      model: 'openai/gpt-4o',
+      model: 'sonar-pro',
       messages: [
         { role: 'system', content },
         { role: 'user', content: `User message: "${query}". Extract the request and respond only with a JSON.` },
